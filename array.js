dee6b5945b0afdfa036a6bf907c9796b1a61ef46
Alok prasad Kanu <alokguptagupta82@gmail.com>
2026-07-08 22:33:27 +0545

    Initial commit: Added workshop tasks

---
+++ array.js
@@ -0,0 +1,9 @@
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [6, 7, 8, 9, 0];
+
+let combined = arr1.concat(arr2);       // merge arr1 and arr2
+arr1.push(6);                           // add 6 to arr1
+arr2.pop();                             // remove last element from arr2
+console.log("arr1:", arr1);             // print arr1
+
+let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
